c0654279265b881abb17e032e78feafb9c2fe674
idibocrazy-ctrl <idibocrazy@gmail.com>
2026-05-21 15:23:50 +0545

    Update main.py

--- main.py
+++ main.py
@@ -16,78 +16,111 @@ bot = commands.Bot(
     intents=intents
 )
 
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
+
 @bot.command(name="visits")
 async def visits(ctx, uid: str, region: str):
 
-    api_url = f"http://np2.npcloud.online:2053/visits?uid={uid}&region={region}"
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(api_url) as response:
-            data = await response.json()
-
-    embed = discord.Embed(
-        title="Visit Statistics",
-        color=0x2F3136
+    api_url = (
+        f"http://np2.npcloud.online:2053/visits?uid={uid}&region={region}"
     )
 
-    embed.add_field(
-        name="Nickname",
-        value=data.get("nickname", "N/A"),
-        inline=False
-    )
+    try:
 
-    embed.add_field(
-        name="Region",
-        value=data.get("region", "N/A"),
-        inline=True
-    )
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url) as response:
+                data = await response.json()
 
-    embed.add_field(
-        name="UID",
-        value=data.get("uid", uid),
-        inline=True
-    )
+        embed = discord.Embed(
+            title="Visit Statistics",
+            color=0x2F3136
+        )
 
-    embed.add_field(
-        name="Level",
-        value=data.get("level", "N/A"),
-        inline=True
-    )
+        embed.add_field(
+            name="Nickname",
+            value=data.get("nickname", "N/A"),
+            inline=False
+        )
 
-    embed.add_field(
-        name="Likes",
-        value=data.get("likes", "0"),
-        inline=True
-    )
+        embed.add_field(
+            name="Region",
+            value=data.get("region", "N/A"),
+            inline=True
+        )
 
-    embed.add_field(
-        name="Success",
-        value=data.get("success", "0"),
-        inline=True
-    )
+        embed.add_field(
+            name="UID",
+            value=data.get("uid", uid),
+            inline=True
+        )
 
-    embed.add_field(
-        name="Fail",
-        value=data.get("fail", "0"),
-        inline=True
-    )
+        embed.add_field(
+            name="Level",
+            value=data.get("level", "N/A"),
+            inline=True
+        )
 
-    embed.add_field(
-        name="Credit",
-        value=data.get("credit", "N/A"),
-        inline=False
-    )
+        embed.add_field(
+            name="Likes",
+            value=str(data.get("likes", "0")),
+            inline=True
+        )
 
-    # embed.set_image(url="https://ibb.co/6kLpTh1/standard (8).gif")
-            file = discord.File("assets/standard (8).gif", filename="standard (8).gif")
-            embed.set_image(url="attachment://standard (8).gif")
-        text="DEVELOPED BY DIBOXE LEGIT"
-    )
+        embed.add_field(
+            name="Success",
+            value=str(data.get("success", "0")),
+            inline=True
+        )
 
-    await ctx.send(embed=embed)
+        embed.add_field(
+            name="Fail",
+            value=str(data.get("fail", "0")),
+            inline=True
+        )
+
+        embed.add_field(
+            name="Credit",
+            value=data.get("credit", "N/A"),
+            inline=False
+        )
+
+        # Attach GIF
+        file = discord.File(
+            "assets/standard (8).gif",
+            filename="standard.gif"
+        )
+
+        # Set GIF inside embed
+        embed.set_image(
+            url="attachment://standard.gif"
+        )
+
+        # User avatar as thumbnail
+        embed.set_thumbnail(
+            url=ctx.author.avatar.url
+            if ctx.author.avatar
+            else ctx.author.default_avatar.url
+        )
+
+        embed.set_footer(
+            text="DEVELOPED BY DIBOXE LEGIT •"
+        )
+
+        await ctx.send(
+            f"{ctx.author.mention}",
+            embed=embed,
+            file=file
+        )
+
+    except Exception as e:
+        print(e)
+        await ctx.send(
+            f"{ctx.author.mention} ❌ Failed to fetch visit data."
+        )
+
 
 bot.run(TOKEN)
